710122ec1a936dde4cef83d798a9cb79eaf63555
eunoo1995 <eunoo1995@gmail.com>
2023-03-14 18:40:51 +0900

    [#2] Feat: chart.js 모듈 분리 및 옵션, 데이터 반환 함수 구현

---
+++ src/utils/chart.ts
@@ -0,0 +1,114 @@
+import {
+  Chart as ChartJS,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+  Filler,
+} from 'chart.js';
+
+import { ChartData } from '@/types';
+
+ChartJS.register(
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+  Filler,
+);
+
+Tooltip.positioners.custom = function (_, eventPosition) {
+  return {
+    x: eventPosition.x,
+    y: eventPosition.y,
+  };
+};
+
+export const getOptions = (datas: ChartData[]) => ({
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+    tooltip: {
+      position: 'custom' as const,
+      callbacks: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        title: function (context: any) {
+          return `id: ${datas[context[0].dataIndex].id}`;
+        },
+      },
+    },
+  },
+  interaction: {
+    mode: 'x' as const,
+  },
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+    },
+    area: {
+      type: 'linear' as const,
+      display: true,
+      position: 'left' as const,
+    },
+    bar: {
+      type: 'linear' as const,
+      display: true,
+      position: 'right' as const,
+      min: 10000,
+    },
+  },
+  parsing: {
+    yAxisKey: 'value',
+  },
+});
+
+export const getFilteredData = (labels: string[], datas: ChartData[]) => {
+  const themes = {
+    성북구: 'rgb(255, 139, 139)',
+    강남구: 'rgb(177, 254, 139)',
+    노원구: 'rgb(136, 255, 255)',
+    중랑구: 'rgb(216, 152, 255)',
+  };
+
+  return {
+    labels,
+    datasets: [
+      {
+        type: 'line' as const,
+        label: 'area',
+        borderColor: 'rgb(167, 167, 167,0.5)',
+        backgroundColor: 'rgba(218, 218, 218, 0.2)',
+        pointBorderColor: 'rgba(256,256,256,0)',
+        pointBackgroundColor: 'rgba(256,256,256,0)',
+        borderWidth: 2,
+        fill: true,
+        data: datas.map((data) => data.value_area),
+        tension: 0.3,
+        yAxisID: 'area',
+      },
+      {
+        type: 'bar' as const,
+        label: 'bar',
+        borderColor: 'white',
+        borderWidth: 2,
+        backgroundColor: datas.map((data) => themes[data.id as keyof typeof themes]),
+        data: datas.map((data) => data.value_bar),
+        yAxisID: 'bar',
+      },
+    ],
+  };
+};
