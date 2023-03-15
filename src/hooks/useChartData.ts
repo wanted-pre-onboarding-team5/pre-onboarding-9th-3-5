dcb5c27f57ed454050036ffb5454e4c2edcce3a3
letsjo <hyunoh.jo@gmail.com>
2023-03-15 19:20:14 +0900

    [#2] Refactor: hook 반환값 변경

--- src/hooks/useChartData.ts
+++ src/hooks/useChartData.ts
@@ -1,19 +1,23 @@
+import { ChartData, ChartOptions } from 'chart.js';
 import { useEffect, useState } from 'react';
 
 import { API_PATH } from '@/constants';
 import { httpClient, createChartData } from '@/utils';
 
 export const useChartData = () => {
-  const [chartData, setChartData] = useState();
+  const [data, setData] = useState<ChartData>();
+  const [options, setOptions] = useState<ChartOptions>();
 
   useEffect(() => {
     const fetchData = async () => {
       const { response } = await httpClient(API_PATH.data);
-      setChartData(createChartData(response));
+      const chartData = createChartData(response);
+      setData(chartData.data);
+      setOptions(chartData.options);
     };
 
     fetchData();
-  }, [setChartData]);
+  }, [setData, setOptions]);
 
-  return chartData;
+  return [data, options];
 };
