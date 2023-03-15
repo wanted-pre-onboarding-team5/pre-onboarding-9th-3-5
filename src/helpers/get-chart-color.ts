29ee787889f10fdaf68931c6998fca8514754ed2
Seok-CH <tjr6677@gmail.com>
2023-03-15 13:29:11 +0900

    [#2] Refactor: 색상 맵핑 상수화
    - 조건문으로 처리하던 부분을 따로 상수화하여 처리하였습니다

--- src/helpers/get-chart-color.ts
+++ src/helpers/get-chart-color.ts
@@ -1,15 +1,17 @@
-const getChartColor = (idArray: string[]) => {
+const colorMap = {
+  중랑구: 'red',
+  노원구: 'yellow',
+  성북구: 'Green',
+  강남구: 'blue',
+} as const;
+
+type KeyOfColorMap = keyof typeof colorMap;
+
+const getChartColor = (idArray: KeyOfColorMap[]) => {
   const colorArray = [];
 
   for (const id of idArray) {
-    let color = 'gray';
-
-    if (id === '중랑구') color = 'red';
-    else if (id === '노원구') color = 'yellow';
-    else if (id === '성북구') color = 'green';
-    else if (id === '강남구') color = 'blue';
-
-    colorArray.push(color);
+    colorArray.push(colorMap[id]);
   }
 
   return colorArray;
