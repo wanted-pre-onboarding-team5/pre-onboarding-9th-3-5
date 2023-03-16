185fe6f7e3c1b6ddc9316a667997d992541cd9f9
Jayun <jay.lee.2280@gmail.com>
2023-03-16 12:22:27 +0900

    [#2] Refactor: chart util 함수에 타입 적용

---
+++ src/constants/chart.ts
@@ -0,0 +1,4 @@
+export const Y_LABEL = {
+  area: 'value_area',
+  bar: 'value_bar',
+} as const;
--- src/utils/chart.ts
+++ src/utils/chart.ts
@@ -1,12 +1,25 @@
+import { Y_LABEL } from '@/constants';
+
+import type { ChartData, ChartOptions, ScriptableContext, ChartType } from 'chart.js';
+
+import { ResponseData } from '@/types/ResponseDataType';
+
+type YLabel = (typeof Y_LABEL)[keyof typeof Y_LABEL];
+type ChartContext = ScriptableContext<ChartType>;
+
 export const getKeysFromObj = (obj = {}) => {
   return Object.keys(obj);
 };
 
-export const convert = (data, keys, keyName) => {
-  return keys.map((key) => data[key][keyName]);
+export const extractValueFromResponse = (
+  responseData: ResponseData,
+  xLabels: string[],
+  yLabel: YLabel,
+) => {
+  return xLabels.map((xLabel) => responseData[xLabel][yLabel]);
 };
 
-export const getChartData = (labels, data1, data2) => {
+export const getChartData = (labels: string[], data1: number[], data2: number[]): ChartData => {
   return {
     labels,
     datasets: [
@@ -14,11 +27,11 @@ export const getChartData = (labels, data1, data2) => {
         type: 'line' as const,
         label: 'Value_Area',
         data: data1,
-        borderColor: (context) => {
+        borderColor: (context: ChartContext) => {
           const { active } = context;
           return active ? 'purple' : 'rgb(227, 179, 242, 0.8)';
         },
-        backgroundColor: (context) => {
+        backgroundColor: (context: ChartContext) => {
           const { active } = context;
           return active ? 'purple' : 'rgb(227, 179, 242, 0.8)';
         },
@@ -29,8 +42,9 @@ export const getChartData = (labels, data1, data2) => {
         type: 'bar' as const,
         label: 'Value_Bar',
         data: data2,
-        backgroundColor: (context) => {
+        backgroundColor: (context: ChartContext) => {
           const { active } = context;
+          if (active) return 'yellow';
           return active ? 'yellow' : 'rgba(233, 227, 157, 0.516)';
         },
         yAxisID: 'y2',
@@ -39,7 +53,7 @@ export const getChartData = (labels, data1, data2) => {
   };
 };
 
-export const getChartOptions = (responseData) => {
+export const getChartOptions = (responseData: ResponseData): ChartOptions => {
   return {
     responsive: true,
     plugins: {
@@ -52,7 +66,7 @@ export const getChartOptions = (responseData) => {
       },
       tooltip: {
         callbacks: {
-          label: function ({ label }) {
+          label: function ({ label }: { label: string }) {
             const { id, value_area, value_bar } = responseData[label];
             const text = `[${id}] 🟣Value_Area: ${value_area} 🟡Value_Bar: ${value_bar}`;
             return text;
@@ -63,15 +77,9 @@ export const getChartOptions = (responseData) => {
     scales: {
       y1: {
         position: 'left',
-        ticks: {
-          beginAtZero: true,
-        },
       },
       y2: {
         position: 'right',
-        ticks: {
-          beginAtZero: false,
-        },
       },
     },
   };
