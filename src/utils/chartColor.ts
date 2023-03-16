9b9722b0302da006f79c9c6e826800d234dc1c2d
Jayun <jay.lee.2280@gmail.com>
2023-03-16 15:16:50 +0900

    [#3] Refactor: 색깔 상수화 적용하여 유틸함수 리팩토링

--- src/utils/chartColor.ts
+++ src/utils/chartColor.ts
@@ -1,14 +1,9 @@
+import { CHART_BG_COLOR } from '@/constants';
+
 import { ResponseData } from '@/types/ResponseDataType';
 
 export const fillChartColorById = (chartData: ResponseData, responseDataKey: string) => {
-  const chartBgColorMap = {
-    성북구: 'rgba(255, 99, 132, 0.5)',
-    중랑구: 'rgba(54, 162, 235, 0.5)',
-    강남구: 'rgba(255, 206, 86, 0.5)',
-    노원구: 'rgba(75, 192, 192, 0.5)',
-  } as const;
-
   const placeKey = chartData[responseDataKey].id;
 
-  return chartBgColorMap[placeKey] || 'grey';
+  return CHART_BG_COLOR[placeKey] || 'grey';
 };
