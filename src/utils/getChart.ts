bd7ed26732fa766fd8fbff40f1eabc5b15dc6066
hannaax <111215320+hannaax@users.noreply.github.com>
2023-03-16 19:10:15 +0900

    [#3] Refact: 함수 getChartColor 파라미터명 수정

--- src/utils/getChart.ts
+++ src/utils/getChart.ts
@@ -1,10 +1,10 @@
-export const getChartColor = (ids: string[]) => {
-  return ids.map((id) =>
-    id === '중랑구'
+export const getChartColor = (districtValues: string[]) => {
+  return districtValues.map((value) =>
+    value === '중랑구'
       ? 'rgba(87, 224, 142, 1)'
-      : id === '성북구'
+      : value === '성북구'
       ? 'rgba(169, 123, 229, 1)'
-      : id === '강남구'
+      : value === '강남구'
       ? 'rgba(224, 142, 87, 1)'
       : 'rgba(87, 167, 224, 1)',
   );
