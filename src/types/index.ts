3dc64c9086664c75a34ad4168d38918bbd09be50
eunoo1995 <eunoo1995@gmail.com>
2023-03-16 11:56:01 +0900

    [#3] Feat: mock 데이터 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,15 @@ export interface ChartData {
   value_area: number;
   value_bar: number;
 }
+
+export interface responseData {
+  type: string;
+  version: number;
+  response: {
+    [key: string]: {
+      id: string;
+      value_area: number;
+      value_bar: number;
+    };
+  };
+}
